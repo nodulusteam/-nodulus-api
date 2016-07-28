2b79f40f8282de94ac87dd563c42b10f7cce903a
roibh <roi@ewave.co.il>
2016-07-28 23:05:24 +0300

    first commit

---
+++ api.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="typings/main.d.ts" />
+export declare class api {
+    cleanEntityFramework(body: any, level: number): void;
+    getOperator(key: string): any;
+    start(app: any): void;
+}
